d98bb00731f2bbb8f0fd9a9ecc92fdf79915d7c7
Rafail Retzos <rret@corporate.iteam.gr>
2017-08-07 16:44:20 +0300

    Add project files.

---
+++ Migrations/20170807102322_ApplyConstraints.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class ApplyConstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Model_Makes_Makeid",
+                table: "Model");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Model",
+                table: "Model");
+
+            migrationBuilder.RenameTable(
+                name: "Model",
+                newName: "Models");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Model_Makeid",
+                table: "Models",
+                newName: "IX_Models_Makeid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Models",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Makes",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Models",
+                table: "Models",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Models_Makes_Makeid",
+                table: "Models",
+                column: "Makeid",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Models_Makes_Makeid",
+                table: "Models");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Models",
+                table: "Models");
+
+            migrationBuilder.RenameTable(
+                name: "Models",
+                newName: "Model");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Models_Makeid",
+                table: "Model",
+                newName: "IX_Model_Makeid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Model",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Makes",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Model",
+                table: "Model",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Model_Makes_Makeid",
+                table: "Model",
+                column: "Makeid",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
